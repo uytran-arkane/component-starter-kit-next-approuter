acb03fe188c797929ccb8b8278b1513d2c02ac29
Uy Tran <uy.tran@arkanedigital.com>
2025-03-18 10:37:26 -0700

    person editable name

--- src/components/custom-canvas/Person.tsx
+++ src/components/custom-canvas/Person.tsx
@@ -2,7 +2,7 @@ import { FC } from 'react';
 import Image from 'next/image';
 
 import { AssetParamValue } from '@uniformdev/canvas';
-import { ComponentProps, UniformRichText } from '@uniformdev/canvas-next-rsc/component';
+import { ComponentProps, UniformRichText, UniformText } from '@uniformdev/canvas-next-rsc/component';
 
 export type PersonParameters = {
   personName?: string;
@@ -37,7 +37,15 @@ const Person: FC<PersonProps> = ({
     )}
     {(!!personName || !!personRole || !!personEmail || !!personPhone) && (
       <div className="mb-4 flex flex-col gap-1">
-        {!!personName && <h3 className="mb-3 text-center font-display text-h3">{personName}</h3>}
+        {!!personName && (
+          <UniformText
+            component={component}
+            context={context}
+            parameterId="personName"
+            as="h3"
+            className='mb-3 text-center font-display text-h3"'
+          />
+        )}
         {!!personRole && (
           <h4 className="text-center text-[18px] font-semibold uppercase text-brand-purple-600">{personRole}</h4>
         )}
